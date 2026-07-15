1a38764f8a20546edb4daf7e6d1738e8011ff254
aaweaver44 <aaweaver@ncsu.edu>
2026-07-15 10:11:40 -0400

    Recover Cartesian textbook baseline from history; add gitignore

--- cartesian baseline - textbook heat equation/reaction_diffusion.m
+++ cartesian baseline - textbook heat equation/reaction_diffusion.m
@@ -1,6 +1,6 @@
 clear all; close all;
     %%%__Initialize parameters, grid, & ICs__%%%
-D = 10; % diffusion coefficient (cm^2/s)
+D = 1; % diffusion coefficient (cm^2/s)
 k = 1; % nutrients feeding the cell (1/s) where negative = consumption, positive = influx
 L = 1; % domain length (cm)
 
@@ -13,16 +13,16 @@ x = linspace(x_start, x_end, Nx+1); % matrix of spatial grid
 
 t_start = 0;
 t_end = 1; % sec; end simulation after this many seconds have elapsed
-dt = 0.01; % time step size (sec)
+dt = 0.0055; % time step size (sec)
 Nt = round(t_end/dt); % number of time steps 
 t_matrix = linspace(t_start, t_end, Nt+1); % matrix of time steps
 
 c_matrix = zeros(Nx, Nt); % Initialize concentration matrix
 % Initial Conditions
 c0 = 1; % initial concentration
-lbc = 1; % left boundary condition
-rbc = 1; % right boundary condition
-IC = @(x)c0+ 0*x; % initial condition global fcn ; u(x,0)=c0 (same size as inpout)
+lbc = 0; % left boundary condition
+rbc = 0; % right boundary condition
+IC = @(x)sin(2*pi*x).^2; % initial condition global fcn ; u(x,0)=c0 (same size as inpout)
 BC_L = @(t)lbc*t; % left BC global fcn ; u(0,t)=lbc
 BC_R = @(t)rbc*t; % right BC global fcn ; u(1,t)=rbc
 
@@ -31,9 +31,13 @@ BC_R = @(t)rbc*t; % right BC global fcn ; u(1,t)=rbc
 % Backward Euler (implicit)
 
 % Crank-Nicolson / trapezoid
-crank_matrix = crank_nicholson_textbook(x_start, x_end, t_start, t_end, Nx, Nt, D, IC, BC_L, BC_R); % contains a matrix of u(x,t) (spatial by time)
+figure;
+crank_matrix = crank_nicholson_textbook(x_start, x_end, t_start, t_end, Nx, Nt, D, IC, BC_L, BC_R, k); % contains a matrix of u(x,t) (spatial by time)
 
 % Runge-Kutta
 
 % Forward Euler (explicit)
+figure;
+forward_matrix = forward_difference_textbook(x_start, x_end, t_start, t_end, Nx, Nt, D, IC, BC_L, BC_R ,k); % contains a matrix of u(x,t) (spatial by time)
+
 
