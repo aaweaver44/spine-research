c00208658dd0a751b58e03951cbdb0a840815144
aaweaver44 <aaweaver@ncsu.edu>
2026-07-15 10:47:35 -0400

    File renames; prototype name conflict removed

--- Spherical prototype/no spatial constant/reaction_diffusion_spherical_nsc.m
+++ Spherical prototype/no spatial constant/reaction_diffusion_spherical_nsc.m
@@ -42,12 +42,12 @@ BC_R = @(t) rbc * ones(size(t));
 
 % Crank-Nicolson (implicit, spherical)
 figure;
-crank_matrix = crank_nicholson_spherical_Claude(r_start, r_end, t_start, t_end, ...
+crank_matrix = crank_nicholson_spherical_nsc(r_start, r_end, t_start, t_end, ...
                                           Nr, Nt, D, IC, BC_R, k);
 
 % Forward Euler (explicit, spherical)
 figure;
-forward_matrix = forward_difference_spherical_Claude(r_start, r_end, t_start, t_end, ...
+forward_matrix = forward_difference_spherical_nsc(r_start, r_end, t_start, t_end, ...
                                                Nr, Nt, D, IC, BC_R, k);
 
 %%%__Compare the two methods at final time (line plot)__%%%
